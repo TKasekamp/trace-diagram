29a24babcfaf9b692860f577951617482da92112
Tõnis Kasekamp <toniskasekamp@gmail.com>
2017-01-22 13:23:21 +0200

    JS that turns opacity to 0.

--- js/tooltips.js
+++ js/tooltips.js
@@ -1,21 +1,21 @@
 /**
  * Created by Tonis on 18.11.2016.
  */
-// On tooltip
-var onT = false;
-// on element
-var onE = false;
-
-var reset = function () {
-    console.log("reset");
-    onT = false;
-    onE = false;
-}
+// // On tooltip
+// var onT = false;
+// // on element
+// var onE = false;
+//
+// var reset = function () {
+//     console.log("reset");
+//     onT = false;
+//     onE = false;
+// }
 var tooltipsArray = function () {
     // Array of arrays
     var tooltips = [];
     var tooltip_count = 22;
-    for (i=1; i < tooltip_count; i++) {
+    for (i = 1; i < tooltip_count; i++) {
         // SVG element ID, tooltip id
         var tooltip = ["#element" + i, "#tooltip" + i];
         tooltips.push(tooltip);
@@ -25,40 +25,20 @@ var tooltipsArray = function () {
 
 var registerTooltip = function (id, divId, svgAll) {
 
-    var div = toolTipListener(divId);
+    var div = d3.select(divId)
+        .style("opacity", 0).style("pointer-events", "none");
 
     svgAll.select(id)
         .on("mouseenter", function (d) {
-            console.log("new loop");
             div.transition()
                 .duration(200)
                 .style("opacity", 1);
             div
-                .style("pointer-events", "auto")
                 .style("left", (d3.event.pageX) + "px")
                 .style("top", (d3.event.pageY + 30) + "px");
 
-            onT = true;
-            console.log("ont " + onT);
-            onE = true;
-            console.log("one " + onE);
         })
-        .on("mouseleave", function (d) {
-            if (onE && !onT) {
-                div.transition()
-                    .duration(500)
-                    .style("opacity", 0)
-                    .style("pointer-events", "none");
-                console.log("hiding tip in reg tooltip");
-                reset();
-
-            }
-
-            // div
-            //     .style("pointer-events", "auto");
-            onE = false;
-            console.log("one " + onE);
-        });
+    ;
 
 };
 
@@ -86,39 +66,6 @@ var registerTooltipNoScroll = function (id, divId, svgAll) {
 
 };
 
-var toolTipListener = function(divId) {
-    var div = d3.select(divId)
-        .style("opacity", 0);
-
-    div.on("mouseenter", function (d) {
-        console.log("on tooltip");
-        div
-            .style("pointer-events", "auto");
-        // onT = true;
-        // console.log("ont " + onT);
-
-    })
-        .on("mouseleave", function (d) {
-            console.log("div mouseout");
-            if (onT && !onE) {
-                div.transition()
-                    .duration(500)
-                    .style("opacity", 0)
-                    .style("pointer-events", "none");
-                console.log("hiding tip div itself");
-                reset();
-
-
-            }
-            onT = false;
-            console.log("ont " + onT);
-            // onE = false;
-            // console.log("one " + onE);
-        });
-
-    return div;
-}
-
 
 var generateTooltips = function () {
 
@@ -130,6 +77,23 @@ var generateTooltips = function () {
     }
 };
 
+// var closeTooltip = function (e) {
+//     console.log(e);
+//     var par = $(event.target).parent().parent();
+//     console.log(par);
+//
+//     par[1].setAttribute('style','opacity:0');
+//     // e = e || window.event;
+//     // var targ = e.target || e.srcElement;
+//     // console.log(targ);
+//     // if (targ.nodeType == 3) targ = targ.parentNode; // defeat Safari bug
+// }
+
+var closeTooltip = function (id) {
+    d3.select("#" + id)
+        .style("opacity", 0).style("pointer-events", "none");
+
+}
 
 jQuery(document).ready(function () {
     // On load listener NECESSARY

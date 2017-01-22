7aae2b57606402c62f10f4e12a766da68d763fc9
Tõnis Kasekamp <toniskasekamp@gmail.com>
2017-01-22 13:47:16 +0200

    Button makes tooltip disappear

--- js/tooltips.js
+++ js/tooltips.js
@@ -1,30 +1,35 @@
 /**
  * Created by Tonis on 18.11.2016.
  */
-// // On tooltip
-// var onT = false;
-// // on element
-// var onE = false;
-//
-// var reset = function () {
-//     console.log("reset");
-//     onT = false;
-//     onE = false;
-// }
+// Tooltips with close button
 var tooltipsArray = function () {
     // Array of arrays
     var tooltips = [];
-    var tooltip_count = 22;
+    var tooltip_count = 2;
     for (i = 1; i < tooltip_count; i++) {
+        // SVG element ID, tooltip id
+        console.log(i);
+        var tooltip = ["#element" + i, "#tooltip" + i];
+        tooltips.push(tooltip);
+    }
+    return tooltips;
+};
+
+// For tooltips with no scroll
+var tooltipsArray2 = function () {
+    // Array of arrays
+    var tooltips = [];
+    var tooltip_count = 22;
+    for (i = 2; i < tooltip_count; i++) {
         // SVG element ID, tooltip id
         var tooltip = ["#element" + i, "#tooltip" + i];
         tooltips.push(tooltip);
     }
     return tooltips;
-}
+};
 
 var registerTooltip = function (id, divId, svgAll) {
-
+    console.log(id);
     var div = d3.select(divId)
         .style("opacity", 0).style("pointer-events", "none");
 
@@ -34,6 +39,7 @@ var registerTooltip = function (id, divId, svgAll) {
                 .duration(200)
                 .style("opacity", 1);
             div
+                .style("pointer-events", "auto")
                 .style("left", (d3.event.pageX) + "px")
                 .style("top", (d3.event.pageY + 30) + "px");
 
@@ -71,29 +77,26 @@ var generateTooltips = function () {
 
     var svgAll = d3.select(document.getElementById("alphasvg").getSVGDocument()).selectAll("g");
 
+    // With close button
     var tooltips = tooltipsArray();
     for (i = 0; i < tooltips.length; i++) {
+        console.log("sdgsdfse");
         registerTooltip(tooltips[i][0], tooltips[i][1], svgAll);
+    }
+
+    // No close button
+    var tooltips2 = tooltipsArray2();
+    for (i = 0; i < tooltips2.length; i++) {
+        registerTooltipNoScroll(tooltips2[i][0], tooltips2[i][1], svgAll);
     }
 };
 
-// var closeTooltip = function (e) {
-//     console.log(e);
-//     var par = $(event.target).parent().parent();
-//     console.log(par);
-//
-//     par[1].setAttribute('style','opacity:0');
-//     // e = e || window.event;
-//     // var targ = e.target || e.srcElement;
-//     // console.log(targ);
-//     // if (targ.nodeType == 3) targ = targ.parentNode; // defeat Safari bug
-// }
 
 var closeTooltip = function (id) {
     d3.select("#" + id)
         .style("opacity", 0).style("pointer-events", "none");
 
-}
+};
 
 jQuery(document).ready(function () {
     // On load listener NECESSARY

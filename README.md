94a567263fac8ec141fe47d3b4e1321b20e5f7aa
Umesh .A Bhat <umesh@ut.ee>
2017-02-24 16:50:58 +0200

    Updated to.svg diagram

--- README.md
+++ README.md
@@ -1,17 +1,2 @@
 # trace-diagram
 FRM4SOC traceability chain diagram
-
-Run the project locally:
-python -m http.server 9000
-
-# Colour palette
-Red: #FB1223
-Green: #50d700
-Background: #e5e5e5
-
-Green box: #ABEA85
-Red box: #FF2B15
-
-
-https://frm4soc.org/index.php/diagram/
-https://frm4soc.org/index.php/about/background-of-frm/
--- js/tooltips.js
+++ js/tooltips.js
@@ -1,98 +1,111 @@
 /**
  * Created by Tonis on 18.11.2016.
  */
-// On tooltip
-var onT = false;
-// on element
-var onE = false;
-var reset = function() {
-    console.log("reset");
-    onT = false;
-    onE = false;
-}
-var tooltipsArray = function() {
+// Tooltips with close button
+var tooltipsArray = function () {
+    // Array of arrays
+    var tooltips = [];
+    var tooltip_count = 2;
+    for (i = 1; i < tooltip_count; i++) {
+        // SVG element ID, tooltip id
+        console.log(i);
+        var tooltip = ["#element" + i, "#tooltip" + i];
+        tooltips.push(tooltip);
+    }
+
+    var tooltip = ["#element2", "#tooltip1a"];
+    tooltips.push(tooltip);
+    return tooltips;
+};
+
+// For tooltips with no scroll
+var tooltipsArray2 = function () {
     // Array of arrays
     var tooltips = [];
     var tooltip_count = 22;
-    for (i = 1; i < tooltip_count; i++) {
+    for (i = 3; i < tooltip_count; i++) {
         // SVG element ID, tooltip id
         var tooltip = ["#element" + i, "#tooltip" + i];
         tooltips.push(tooltip);
     }
     return tooltips;
-}
-var registerTooltip = function(id, divId, svgAll) {
-    var div = toolTipListener(divId);
-    svgAll.select(id).on("mouseenter", function(d) {
-        console.log("new loop");
-        div.transition().duration(200).style("opacity", 1);
-        div.style("pointer-events", "auto").style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY + 30) + "px");
-        onT = true;
-        console.log("ont " + onT);
-        onE = true;
-        console.log("one " + onE);
-    }).on("mouseleave", function(d) {
-        if (onE && !onT) {
-            div.transition().duration(500).style("opacity", 0).style("pointer-events", "none");
-            console.log("hiding tip in reg tooltip");
-            reset();
-        }
-        // div
-        //     .style("pointer-events", "auto");
-        onE = false;
-        console.log("one " + onE);
-    });
 };
+
+var registerTooltip = function (id, divId, svgAll) {
+    console.log(id);
+    var div = d3.select(divId);
+
+    svgAll.select(id)
+        .on("mouseenter", function (d) {
+            // Reset
+            d3.selectAll(".tooltip-painter").style("opacity", 0).style("height", 0);
+
+            div.transition()
+                .duration(200)
+                .style("height", "auto")
+                .style("opacity", 1);
+        })
+    ;
+
+};
+
 // For tooltips with no scroll
-var registerTooltipNoScroll = function(id, divId, svgAll) {
-    var div = d3.select(divId).style("opacity", 0).style("pointer-events", "none");
-    svgAll.select(id).on("mouseenter", function(d) {
-        div.transition().duration(200).style("opacity", 1);
-        div.style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY + 30) + "px");
-    }).on("mouseleave", function(d) {
-        div.transition().duration(500).style("opacity", 0);
-    });
+var registerTooltipNoScroll = function (id, divId, svgAll) {
+
+    var div = d3.select(divId)
+        .style("opacity", 0).style("pointer-events", "none");
+
+    svgAll.select(id)
+        .on("mouseenter", function (d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", 1);
+            div
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY + 30) + "px");
+
+        })
+        .on("mouseleave", function (d) {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
+
 };
-var toolTipListener = function(divId) {
-    var div = d3.select(divId).style("opacity", 0);
-    div.on("mouseenter", function(d) {
-        console.log("on tooltip");
-        div.style("pointer-events", "auto");
-        // onT = true;
-        // console.log("ont " + onT);
-    }).on("mouseleave", function(d) {
-        console.log("div mouseout");
-        if (onT && !onE) {
-            div.transition().duration(500).style("opacity", 0).style("pointer-events", "none");
-            console.log("hiding tip div itself");
-            reset();
-        }
-        onT = false;
-        console.log("ont " + onT);
-        // onE = false;
-        // console.log("one " + onE);
-    });
-    return div;
-}
-var generateTooltips = function() {
+
+
+var generateTooltips = function () {
+
     var svgAll = d3.select(document.getElementById("alphasvg").getSVGDocument()).selectAll("g");
+
+    // With close button
     var tooltips = tooltipsArray();
     for (i = 0; i < tooltips.length; i++) {
+        console.log("sdgsdfse");
         registerTooltip(tooltips[i][0], tooltips[i][1], svgAll);
     }
+
+
+    // No close button
+    var tooltips2 = tooltipsArray2();
+    for (i = 0; i < tooltips2.length; i++) {
+        registerTooltipNoScroll(tooltips2[i][0], tooltips2[i][1], svgAll);
+    }
+
+    // Set first tooltip open
+    d3.select(tooltips[0][1]).style("opacity", 1).style("height", "auto");
 };
-jQuery(document).ready(function() {
+
+
+jQuery(document).ready(function () {
     // On load listener NECESSARY
     var svgholder = jQuery('body').find("object#alphasvg");
-    svgholder.load("image/svg+xml", function() {
+
+    svgholder.load("image/svg+xml", function () {
         generateTooltips();
-        //gen3();
     });
-    panZoomInstance = svgPanZoom('#to-svg', {
-        zoomEnabled: true,
-        controlIconsEnabled: true,
-        fit: true,
-        center: true,
-        minZoom: 0.1
-    });
+
 });
+
+
+
